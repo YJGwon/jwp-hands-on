0972864777d93edad9de6c079ec03f83072032cc
Forky <trulyforky@gmail.com>
2022-09-02 17:47:31 +0900

    [HTTP 활용하기 미션] Cache Busting 구현 (#1)
    * docs(cache/README.md): 기능 요구사항 작성 및 최상위 README에 링크 추가
    
    * feat: 모든 요청에 대한 응답 `Cache-Control` 기본값 `no-cache, private` 적용
    
    * chore: 응답 압축 설정 적용
    
    * feat: `/etag`요청 시 ETag 적용
    
    * feat: 정적 리소스 Cache Busting 적용

--- cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
+++ cache/src/main/java/com/example/etag/EtagFilterConfiguration.java
@@ -1,12 +1,29 @@
 package com.example.etag;
 
+import static com.example.version.CacheBustingWebConfig.PREFIX_STATIC_RESOURCES;
+
+import com.example.version.ResourceVersion;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @Configuration
 public class EtagFilterConfiguration {
 
-//    @Bean
-//    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
-//        return null;
-//    }
+    private final ResourceVersion version;
+
+    @Autowired
+    public EtagFilterConfiguration(final ResourceVersion version) {
+        this.version = version;
+    }
+
+    @Bean
+    public FilterRegistrationBean<ShallowEtagHeaderFilter> shallowEtagHeaderFilter() {
+        final FilterRegistrationBean<ShallowEtagHeaderFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new ShallowEtagHeaderFilter());
+        registration.addUrlPatterns("/etag", PREFIX_STATIC_RESOURCES + "/" + version.getVersion() + "/*");
+        return registration;
+    }
 }
