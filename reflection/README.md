215e6e5093e05f5029fbbd389bc5ef4df3c1ec64
Forky <trulyforky@gmail.com>
2022-09-19 01:10:06 +0900

    [자바 리플렉션 사용하기] 학습 테스트 구현 (#4)
    * feat: Junit3TestRunner 구현
    
    * feat: Junit3TestRunner 구현
    
    * feat: ReflectionTest 구현
    
    * feat: ReflectionsTest 구현

---
+++ reflection/README.md
@@ -0,0 +1,30 @@
+# Reflection
+
+[📖 자바 리플렉션 사용하기](https://techcourse.woowahan.com/s/cCM7rQR9/ls/VKABLQsK)
+
+### 학습한 내용
+- [Reflection](https://forky-freeky-forky.notion.site/Reflection-60a859ea1f7946ba99d8d109e3e9244c)
+
+---
+
+### Junit3TestRunner
+- [X] 이름으로 method를 찾아 실행한다.
+
+### Junit4TestRunner
+- [X] annotation으로 method를 찾아 실행한다.
+
+### ReflectionTest
+class에 대해
+- [X] 이름을 구한다.
+- [X] 선언된 field 이름을 구한다.
+- [X] 선언된 method 이름을 구한다.
+- [X] 모든 생성자를 구한다.
+- [X] Constructor 객체로 instance를 생성한다.
+- [X] public field를 구한다.
+- [X] 선언된 모든 field를 구한다.
+- [X] 이름으로 특정 field를 찾는다.
+- [X] field의 type을 구한다.
+- [X] instance의 private field에 접근하여 값을 수정한다.
+
+### ReflectionsTest
+- [X] example package 내에서 `@Controller`, `@Service`, `@Repository` annotation을 가진 class를 찾아 log로 남긴다.
--- reflection/src/test/java/reflection/Junit4TestRunner.java
+++ reflection/src/test/java/reflection/Junit4TestRunner.java
@@ -1,13 +1,24 @@
 package reflection;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class Junit4TestRunner {
 
     @Test
     void run() throws Exception {
-        Class<Junit4Test> clazz = Junit4Test.class;
+        final Class<Junit4Test> clazz = Junit4Test.class;
 
-        // TODO Junit4Test에서 @MyTest 애노테이션이 있는 메소드 실행
+        final List<Method> testMethods = Arrays.stream(clazz.getDeclaredMethods())
+                .filter(method -> method.isAnnotationPresent(MyTest.class))
+                .collect(Collectors.toList());
+
+        final Junit4Test junit4Test = clazz.getDeclaredConstructor().newInstance();
+        for (Method method : testMethods) {
+            method.invoke(junit4Test);
+        }
     }
 }
