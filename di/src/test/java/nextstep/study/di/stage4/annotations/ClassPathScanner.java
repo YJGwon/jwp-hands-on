a7019ca65ab6c3def6e988a84523fa51c4478637
YJGwon <yj970125@gmail.com>
2022-09-27 21:51:58 +0900

    feat: Stage4Test 구현

--- di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
@@ -1,10 +1,15 @@
 package nextstep.study.di.stage4.annotations;
 
 import java.util.Set;
+import org.reflections.Reflections;
 
 public class ClassPathScanner {
 
     public static Set<Class<?>> getAllClassesInPackage(final String packageName) {
-        return null;
+        final Reflections reflections = new Reflections(packageName);
+        final Set<Class<?>> repositoryClasses = reflections.getTypesAnnotatedWith(Repository.class);
+        final Set<Class<?>> serviceClasses = reflections.getTypesAnnotatedWith(Service.class);
+        repositoryClasses.addAll(serviceClasses);
+        return repositoryClasses;
     }
 }
--- di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/DIContainer.java
@@ -1,6 +1,11 @@
 package nextstep.study.di.stage4.annotations;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * 스프링의 BeanFactory, ApplicationContext에 해당되는 클래스
@@ -10,15 +15,62 @@ class DIContainer {
     private final Set<Object> beans;
 
     public DIContainer(final Set<Class<?>> classes) {
-        this.beans = Set.of();
+        this.beans = instantiateClasses(classes);
+        assignFieldsToBeans(beans);
     }
 
     public static DIContainer createContainerForPackage(final String rootPackageName) {
-        return null;
+        final Set<Class<?>> classes = ClassPathScanner.getAllClassesInPackage(rootPackageName);
+        return new DIContainer(classes);
     }
 
     @SuppressWarnings("unchecked")
     public <T> T getBean(final Class<T> aClass) {
-        return null;
+        return (T) beans.stream()
+                .filter(bean -> aClass.isAssignableFrom(bean.getClass()))
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("해당하는 bean을 찾지 못했습니다: " + aClass.getName()));
+    }
+
+    private Set<Object> instantiateClasses(final Set<Class<?>> classes) {
+        return classes.stream()
+                .map(this::instantiateClass)
+                .collect(Collectors.toSet());
+    }
+
+    private <T> T instantiateClass(final Class<T> clazz) {
+        try {
+            final Constructor<T> defaultConstructor = clazz.getDeclaredConstructor();
+            defaultConstructor.setAccessible(true);
+            return defaultConstructor.newInstance();
+        } catch (InstantiationException | IllegalAccessException |
+                 InvocationTargetException | NoSuchMethodException e) {
+            throw new IllegalArgumentException("bean을 초기화할 수 없습니다: " + clazz.getName());
+        }
+    }
+
+    private void assignFieldsToBeans(final Set<Object> beans) {
+        beans.forEach(bean -> assignFieldsToBean(bean, bean.getClass().getDeclaredFields()));
+    }
+
+    private void assignFieldsToBean(final Object bean, final Field[] fields) {
+        Arrays.stream(fields)
+                .filter(field -> field.isAnnotationPresent(Inject.class))
+                .forEach(field -> assignFieldToBean(bean, field));
+    }
+
+    private void assignFieldToBean(final Object bean, final Field field) {
+        field.setAccessible(true);
+        try {
+            setField(field, bean);
+        } catch (IllegalAccessException e) {
+            throw new UnsupportedOperationException("bean의 필드를 초기화하지 못했습니다: " + field.getName());
+        }
+    }
+
+    private void setField(final Field field, final Object target) throws IllegalAccessException {
+        if (field.get(target) == null) {
+            field.set(target, getBean(field.getType()));
+        }
     }
 }
